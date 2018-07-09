eefce063b0c8e38f674c99c943da2919d415ae30
Martim Marques <martimasterdeveloper@gmail.com>
2018-07-10 00:39:18 +0100

    Create sudoku system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,124 @@
+mod sudoku;
+
+use sudoku::Sudoku;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut column1: [u32; 9] = [0; 9];
+    column1[0] = 0;
+    column1[1] = 0;
+    column1[2] = 0;
+    column1[3] = 2;
+    column1[4] = 0;
+    column1[5] = 0;
+    column1[6] = 0;
+    column1[7] = 4;
+    column1[8] = 0;
+
+    let mut column2: [u32; 9] = [0; 9];
+    column2[0] = 0;
+    column2[1] = 1;
+    column2[2] = 2;
+    column2[3] = 0;
+    column2[4] = 5;
+    column2[5] = 4;
+    column2[6] = 0;
+    column2[7] = 0;
+    column2[8] = 0;
+
+    let mut column3: [u32; 9] = [0; 9];
+    column3[0] = 8;
+    column3[1] = 0;
+    column3[2] = 7;
+    column3[3] = 0;
+    column3[4] = 6;
+    column3[5] = 9;
+    column3[6] = 0;
+    column3[7] = 0;
+    column3[8] = 0;
+
+    let mut column4: [u32; 9] = [0; 9];
+    column4[0] = 3;
+    column4[1] = 0;
+    column4[2] = 4;
+    column4[3] = 0;
+    column4[4] = 0;
+    column4[5] = 2;
+    column4[6] = 0;
+    column4[7] = 9;
+    column4[8] = 0;
+
+    let mut column5: [u32; 9] = [0; 9];
+    column5[0] = 2;
+    column5[1] = 9;
+    column5[2] = 5;
+    column5[3] = 0;
+    column5[4] = 0;
+    column5[5] = 0;
+    column5[6] = 4;
+    column5[7] = 6;
+    column5[8] = 3;
+
+    let mut column6: [u32; 9] = [0; 9];
+    column6[0] = 0;
+    column6[1] = 8;
+    column6[2] = 0;
+    column6[3] = 4;
+    column6[4] = 0;
+    column6[5] = 0;
+    column6[6] = 2;
+    column6[7] = 0;
+    column6[8] = 5;
+
+    let mut column7: [u32; 9] = [0; 9];
+    column7[0] = 0;
+    column7[1] = 0;
+    column7[2] = 0;
+    column7[3] = 1;
+    column7[4] = 6;
+    column7[5] = 0;
+    column7[6] = 5;
+    column7[7] = 0;
+    column7[8] = 9;
+
+    let mut column8: [u32; 9] = [0; 9];
+    column8[0] = 0;
+    column8[1] = 0;
+    column8[2] = 0;
+    column8[3] = 5;
+    column8[4] = 3;
+    column8[5] = 0;
+    column8[6] = 1;
+    column8[7] = 4;
+    column8[8] = 0;
+
+    let mut column9: [u32; 9] = [0; 9];
+    column9[0] = 0;
+    column9[1] = 7;
+    column9[2] = 0;
+    column9[3] = 0;
+    column9[4] = 0;
+    column9[5] = 4;
+    column9[6] = 0;
+    column9[7] = 0;
+    column9[8] = 0;
+
+    let mut columns: [[u32; 9]; 9] = [column1; 9];
+    columns[0] = column1;
+    columns[1] = column2;
+    columns[2] = column3;
+    columns[3] = column4;
+    columns[4] = column5;
+    columns[5] = column6;
+    columns[6] = column7;
+    columns[7] = column8;
+    columns[8] = column9;
+
+    let mut sudoku = Sudoku {
+        columns,
+        lines: columns
+    };
+
+    sudoku.lines = sudoku.get_lines_by_columns();
+    sudoku::print_sudoku(sudoku);
 }
---
+++ src/sudoku.rs
@@ -0,0 +1,211 @@
+extern crate term_painter;
+
+use self::term_painter::Color::Custom;
+use self::term_painter::ToStyle;
+
+pub struct Sudoku {
+    pub columns: [[u32; 9]; 9],
+    pub lines: [[u32; 9]; 9],
+}
+
+impl Sudoku {
+    pub fn _solve(&self) {
+        self.columns.iter().for_each(|column| {
+            column.iter().for_each(|column_index| {
+                println!("{p0}", p0 = column_index);
+            })
+        });
+    }
+
+    pub fn get_lines_by_columns(&self) -> [[u32; 9]; 9] {
+        let mut lines_by_columns: [[u32; 9]; 9] = [[0; 9]; 9];
+
+        let mut line1: [u32; 9] = [0; 9];
+        line1[0] = self.columns[0][0];
+        line1[1] = self.columns[0][1];
+        line1[2] = self.columns[0][2];
+        line1[3] = self.columns[1][0];
+        line1[4] = self.columns[1][1];
+        line1[5] = self.columns[1][2];
+        line1[6] = self.columns[2][0];
+        line1[7] = self.columns[2][1];
+        line1[8] = self.columns[2][2];
+        lines_by_columns[0] = line1;
+
+        let mut line2: [u32; 9] = [0; 9];
+        line2[0] = self.columns[0][3];
+        line2[1] = self.columns[0][4];
+        line2[2] = self.columns[0][5];
+        line2[3] = self.columns[1][3];
+        line2[4] = self.columns[1][4];
+        line2[5] = self.columns[1][5];
+        line2[6] = self.columns[2][3];
+        line2[7] = self.columns[2][4];
+        line2[8] = self.columns[2][5];
+        lines_by_columns[1] = line2;
+
+        let mut line3: [u32; 9] = [0; 9];
+        line3[0] = self.columns[0][6];
+        line3[1] = self.columns[0][7];
+        line3[2] = self.columns[0][8];
+        line3[3] = self.columns[1][6];
+        line3[4] = self.columns[1][7];
+        line3[5] = self.columns[1][8];
+        line3[6] = self.columns[2][6];
+        line3[7] = self.columns[2][7];
+        line3[8] = self.columns[2][8];
+        lines_by_columns[2] = line3;
+
+        let mut line4: [u32; 9] = [0; 9];
+        line4[0] = self.columns[3][0];
+        line4[1] = self.columns[3][1];
+        line4[2] = self.columns[3][2];
+        line4[3] = self.columns[4][0];
+        line4[4] = self.columns[4][1];
+        line4[5] = self.columns[4][2];
+        line4[6] = self.columns[5][0];
+        line4[7] = self.columns[5][1];
+        line4[8] = self.columns[5][2];
+        lines_by_columns[3] = line4;
+
+        let mut line5: [u32; 9] = [0; 9];
+        line5[0] = self.columns[3][3];
+        line5[1] = self.columns[3][4];
+        line5[2] = self.columns[3][5];
+        line5[3] = self.columns[4][3];
+        line5[4] = self.columns[4][4];
+        line5[5] = self.columns[4][5];
+        line5[6] = self.columns[5][3];
+        line5[7] = self.columns[5][4];
+        line5[8] = self.columns[5][5];
+        lines_by_columns[4] = line5;
+
+        let mut line6: [u32; 9] = [0; 9];
+        line6[0] = self.columns[3][6];
+        line6[1] = self.columns[3][7];
+        line6[2] = self.columns[3][8];
+        line6[3] = self.columns[4][6];
+        line6[4] = self.columns[4][7];
+        line6[5] = self.columns[4][8];
+        line6[6] = self.columns[5][6];
+        line6[7] = self.columns[5][7];
+        line6[8] = self.columns[5][8];
+        lines_by_columns[5] = line6;
+
+        let mut line7: [u32; 9] = [0; 9];
+        line7[0] = self.columns[6][0];
+        line7[1] = self.columns[6][1];
+        line7[2] = self.columns[6][2];
+        line7[3] = self.columns[7][0];
+        line7[4] = self.columns[7][1];
+        line7[5] = self.columns[7][2];
+        line7[6] = self.columns[8][0];
+        line7[7] = self.columns[8][1];
+        line7[8] = self.columns[8][2];
+        lines_by_columns[6] = line7;
+
+        let mut line8: [u32; 9] = [0; 9];
+        line8[0] = self.columns[6][3];
+        line8[1] = self.columns[6][4];
+        line8[2] = self.columns[6][5];
+        line8[3] = self.columns[7][3];
+        line8[4] = self.columns[7][4];
+        line8[5] = self.columns[7][5];
+        line8[6] = self.columns[8][3];
+        line8[7] = self.columns[8][4];
+        line8[8] = self.columns[8][5];
+        lines_by_columns[7] = line8;
+
+        let mut line9: [u32; 9] = [0; 9];
+        line9[0] = self.columns[6][6];
+        line9[1] = self.columns[6][7];
+        line9[2] = self.columns[6][8];
+        line9[3] = self.columns[7][6];
+        line9[4] = self.columns[7][7];
+        line9[5] = self.columns[7][8];
+        line9[6] = self.columns[8][6];
+        line9[7] = self.columns[8][7];
+        line9[8] = self.columns[8][8];
+        lines_by_columns[8] = line9;
+
+        return lines_by_columns;
+    }
+
+    fn _get_columns_by_lines(&self) {}
+}
+
+pub fn print_sudoku(sudoku: Sudoku) {
+    println!("{}{}{}",
+             Custom(1).paint("----------"),
+             Custom(2).paint("---------"),
+             Custom(3).paint("----------"),
+    );
+    for (i, line) in sudoku.lines.iter().enumerate() {
+        match i {
+            0 | 1 | 2 => print!("{}", Custom(1).paint("|")),
+            3 | 4 | 5 => print!("{}", Custom(4).paint("|")),
+            6 | 7 | 8 => print!("{}", Custom(5).paint("|")),
+            _ => print!("{}", "|")
+        }
+        for (index, lines_index) in line.iter().enumerate() {
+            match i {
+                0 | 1 | 2 => {
+                    match index {
+                        0 | 1 | 2 => print!(" {} ", Custom(1).paint(lines_index)),
+                        3 | 4 | 5 => print!(" {} ", Custom(2).paint(lines_index)),
+                        6 | 7 | 8 => print!(" {} ", Custom(3).paint(lines_index)),
+                        _ => print!(" {index} ", index=lines_index)
+                    }
+                }
+                3 | 4 | 5 => {
+                    match index {
+                        0 | 1 | 2 => print!(" {} ", Custom(4).paint(lines_index)),
+                        3 | 4 | 5 => print!(" {} ", Custom(7).paint(lines_index)),
+                        6 | 7 | 8 => print!(" {} ", Custom(6).paint(lines_index)),
+                        _ => print!(" {index} ", index=lines_index)
+                    }
+                }
+                6 | 7 | 8 => {
+                    match index {
+                        0 | 1 | 2 => print!(" {} ", Custom(5).paint(lines_index)),
+                        3 | 4 | 5 => print!(" {} ", Custom(10).paint(lines_index)),
+                        6 | 7 | 8 => print!(" {} ", Custom(9).paint(lines_index)),
+                        _ => print!(" {index} ", index=lines_index)
+                    }
+                }
+                _ => print!(" {} ", lines_index)
+            }
+        }
+        match i {
+            0 | 1 => {
+                println!("{}", Custom(3).paint("|"));
+                println!("{}{}{}",
+                         Custom(1).paint("----------"),
+                         Custom(2).paint("---------"),
+                         Custom(3).paint("----------"),
+                );
+            }
+            2 | 3 | 4 | 5 => {
+                println!("{}", Custom(6).paint("|"));
+                println!("{}{}{}",
+                         Custom(4).paint("----------"),
+                         Custom(7).paint("---------"),
+                         Custom(6).paint("----------"),
+                );
+            },
+            6 | 7 | 8 => {
+                println!("{}", Custom(9).paint("|"));
+                println!("{}{}{}",
+                         Custom(5).paint("----------"),
+                         Custom(10).paint("---------"),
+                         Custom(9).paint("----------"),
+                );
+            },
+            _ => {
+                println!("|");
+                println!("-----------------------------");
+            }
+        }
+
+    }
+}
