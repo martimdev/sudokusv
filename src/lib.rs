30d374971f6b5ee194e2eef6203e4c3c26634a68
Martim Marques <martimasterdeveloper@gmail.com>
2018-07-11 01:37:48 +0100

    Simplify code

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,25 @@
-#[macro_use(array)]
-extern crate ndarray;
 extern crate term_painter;
 
-use ndarray::Array2;
 use std::thread;
 use std::time::Duration;
 use term_painter::Color::Custom;
 use term_painter::ToStyle;
 
 pub struct Sudoku {
-    columns: Array2<u32>,
-    horizontal_lines: Array2<u32>,
-    vertical_lines: Array2<u32>,
+    pub columns: [[u32; 9]; 9],
+    pub horizontal_lines: [[u32; 9]; 9],
+    pub vertical_lines: [[u32; 9]; 9],
 }
 
 impl Sudoku {
-    pub fn new(horizontal_lines: Array2<u32>) -> Sudoku {
-        let columns: Array2<u32> = array![
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        ];
-        let vertical_lines: Array2<u32> = array![
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        ];
-        let sudoku_instance = Sudoku {
-            columns,
+    pub fn new(horizontal_lines: [[u32; 9]; 9]) -> Sudoku {
+        let mut sudoku_instance = Sudoku {
+            columns: [[0; 9]; 9],
             horizontal_lines,
-            vertical_lines,
+            vertical_lines: [[0; 9]; 9],
         };
-        //sudoku_instance.columns = sudoku_instance.get_columns_by_horizontal_lines();
-        //sudoku_instance.vertical_lines = sudoku_instance.get_vertical_lines_by_columns();
+        sudoku_instance.columns = sudoku_instance.get_columns_by_horizontal_lines();
+        sudoku_instance.vertical_lines = sudoku_instance.get_vertical_lines_by_columns();
         return sudoku_instance;
     }
 
@@ -55,14 +30,17 @@ impl Sudoku {
         }
     }
 
-    fn get_columns_by_horizontal_lines(&self) -> Array2<u32> {
-        unimplemented!();
+    fn get_columns_by_horizontal_lines(&self) -> [[u32; 9]; 9] {
+        return [[0; 9]; 9];
     }
 
-    fn get_vertical_lines_by_columns(&self) -> Array2<u32> {
-        unimplemented!();
+    fn get_vertical_lines_by_columns(&self) -> [[u32; 9]; 9] {
+        return [[0; 9]; 9];
     }
 
+    pub fn get_horizontal_lines_by_vertical_lines(&self) -> [[u32; 9]; 9] {
+        return [[0; 9]; 9];
+    }
 }
 
 pub fn print_sudoku(sudoku: &Sudoku) {
@@ -71,7 +49,7 @@ pub fn print_sudoku(sudoku: &Sudoku) {
              Custom(2).paint("---------"),
              Custom(3).paint("----------"),
     );
-    for (i, line) in sudoku.horizontal_lines.outer_iter().enumerate() {
+    for (i, line) in sudoku.horizontal_lines.iter().enumerate() {
         match i {
             0 | 1 | 2 => print!("{}", Custom(1).paint("|")),
             3 | 4 | 5 => print!("{}", Custom(4).paint("|")),
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
-#[macro_use(array)]
-extern crate ndarray;
 extern crate sudokusv;
 
-use ndarray::Array2;
 use sudokusv::Sudoku;
 
 fn main() {
-    let horizontal_lines: Array2<u32> = array![
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ];
+    let horizontal_lines = {
+        [
+            [1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0, 0, 0, 0, 0],
+            [3, 0, 0, 0, 0, 0, 0, 0, 0],
+            [4, 0, 0, 0, 0, 0, 0, 0, 0],
+            [5, 0, 0, 0, 0, 0, 0, 0, 0],
+            [6, 0, 0, 0, 0, 0, 0, 0, 0],
+            [7, 0, 0, 0, 0, 0, 0, 0, 0],
+            [8, 0, 0, 0, 0, 0, 0, 0, 0],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+    };
 
     let sudokusv = Sudoku::new(horizontal_lines);
-    sudokusv.solve()
+    sudokusv.solve();
+    sudokusv.get_horizontal_lines_by_vertical_lines();
 }
