124275e73651755984bb9bd34a98f5d75baa046d
Martim Marques <martimasterdeveloper@gmail.com>
2018-07-11 02:15:16 +0100

    Finishing handling functions

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,9 @@ use term_painter::Color::Custom;
 use term_painter::ToStyle;
 
 pub struct Sudoku {
-    pub columns: [[u32; 9]; 9],
-    pub horizontal_lines: [[u32; 9]; 9],
-    pub vertical_lines: [[u32; 9]; 9],
+    columns: [[u32; 9]; 9],
+    horizontal_lines: [[u32; 9]; 9],
+    vertical_lines: [[u32; 9]; 9],
 }
 
 impl Sudoku {
@@ -19,7 +19,7 @@ impl Sudoku {
             vertical_lines: [[0; 9]; 9],
         };
         sudoku_instance.columns = sudoku_instance.get_columns_by_horizontal_lines();
-        sudoku_instance.vertical_lines = sudoku_instance.get_vertical_lines_by_columns();
+        sudoku_instance.vertical_lines = sudoku_instance.get_vertical_lines_by_horizontal_lines();
         return sudoku_instance;
     }
 
@@ -31,15 +31,63 @@ impl Sudoku {
     }
 
     fn get_columns_by_horizontal_lines(&self) -> [[u32; 9]; 9] {
-        return [[0; 9]; 9];
+        [
+            [
+                self.horizontal_lines[0][0], self.horizontal_lines[0][1], self.horizontal_lines[0][2],
+                self.horizontal_lines[1][0], self.horizontal_lines[1][1], self.horizontal_lines[1][2],
+                self.horizontal_lines[2][0], self.horizontal_lines[2][1], self.horizontal_lines[2][2]
+            ],
+            [
+                self.horizontal_lines[0][3], self.horizontal_lines[0][4], self.horizontal_lines[0][5],
+                self.horizontal_lines[1][3], self.horizontal_lines[1][4], self.horizontal_lines[1][5],
+                self.horizontal_lines[2][3], self.horizontal_lines[2][4], self.horizontal_lines[2][5]
+            ],
+            [
+                self.horizontal_lines[0][6], self.horizontal_lines[0][7], self.horizontal_lines[0][8],
+                self.horizontal_lines[1][6], self.horizontal_lines[1][7], self.horizontal_lines[1][8],
+                self.horizontal_lines[2][6], self.horizontal_lines[2][7], self.horizontal_lines[2][8]
+            ],
+            [
+                self.horizontal_lines[3][0], self.horizontal_lines[3][1], self.horizontal_lines[3][2],
+                self.horizontal_lines[4][0], self.horizontal_lines[4][1], self.horizontal_lines[4][2],
+                self.horizontal_lines[5][0], self.horizontal_lines[5][1], self.horizontal_lines[5][2]
+            ],
+            [
+                self.horizontal_lines[3][3], self.horizontal_lines[3][4], self.horizontal_lines[3][5],
+                self.horizontal_lines[4][3], self.horizontal_lines[4][4], self.horizontal_lines[4][5],
+                self.horizontal_lines[5][3], self.horizontal_lines[5][4], self.horizontal_lines[5][5]
+            ],
+            [
+                self.horizontal_lines[3][6], self.horizontal_lines[3][7], self.horizontal_lines[3][8],
+                self.horizontal_lines[4][6], self.horizontal_lines[4][7], self.horizontal_lines[4][8],
+                self.horizontal_lines[5][6], self.horizontal_lines[5][7], self.horizontal_lines[5][8]
+            ],
+            [
+                self.horizontal_lines[6][0], self.horizontal_lines[6][1], self.horizontal_lines[6][2],
+                self.horizontal_lines[7][0], self.horizontal_lines[7][1], self.horizontal_lines[7][2],
+                self.horizontal_lines[8][0], self.horizontal_lines[8][1], self.horizontal_lines[8][2]
+            ],
+            [
+                self.horizontal_lines[6][3], self.horizontal_lines[6][4], self.horizontal_lines[6][5],
+                self.horizontal_lines[7][3], self.horizontal_lines[7][4], self.horizontal_lines[7][5],
+                self.horizontal_lines[8][3], self.horizontal_lines[8][4], self.horizontal_lines[8][5]
+            ],
+            [
+                self.horizontal_lines[6][6], self.horizontal_lines[6][7], self.horizontal_lines[6][8],
+                self.horizontal_lines[7][6], self.horizontal_lines[7][7], self.horizontal_lines[7][8],
+                self.horizontal_lines[8][6], self.horizontal_lines[8][7], self.horizontal_lines[8][8]
+            ],
+        ]
     }
 
-    fn get_vertical_lines_by_columns(&self) -> [[u32; 9]; 9] {
-        return [[0; 9]; 9];
-    }
-
-    pub fn get_horizontal_lines_by_vertical_lines(&self) -> [[u32; 9]; 9] {
-        return [[0; 9]; 9];
+    fn get_vertical_lines_by_horizontal_lines(&self) -> [[u32; 9]; 9] {
+        let mut vertical_lines = [[0; 9]; 9];
+        for line_index in 0..9 {
+            for index in 0..9 {
+                vertical_lines[line_index][index] = self.horizontal_lines[index][line_index];
+            }
+        }
+        vertical_lines
     }
 }
 
